95d2166baacd1a923541bd0bf854623d3a8dc49a
Aidana Orazbay <oraaidana@gmail.com>
2025-11-20 22:17:08 +0500

    Initial Commit

---
+++ finance/financeApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeApp.swift
+//  finance
+//
+//  Created by Aidana Orazbay on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct financeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
