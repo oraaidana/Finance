e05ab518e532644967c5e0589f0ff59d565f4f1c
Aidana Orazbay <oraaidana@gmail.com>
2025-11-21 19:32:51 +0500

    initial commit

---
+++ finance/ChatView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  finance
+//
+//  Created by Aidana Orazbay on 11/20/25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
